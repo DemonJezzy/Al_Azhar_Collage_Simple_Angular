99eadd55d3201ed9a66329c349b4bc83bf3ef2a8
DemonJezzy <nomanhafeez667@gmail.com>
2021-11-18 23:59:18 +0500

    fitst commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent} from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { StaffComponent } from './staff/staff.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full',  redirectTo: '/home'},
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactusComponent},
+  { path: 'staff', component: StaffComponent},
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { StaffComponent } from './staff/staff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    NavbarComponent,
+    ContactusComponent,
+    StaffComponent
   ],
   imports: [
     BrowserModule,
